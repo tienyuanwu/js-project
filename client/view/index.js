f6e1894415240b5590bad7eb08adefe481aef078
Michael Wu <michael.wu@trubuzz.com>
2018-05-03 18:05:34 +0800

    修改標題

--- client/view/index.js
+++ client/view/index.js
@@ -13,5 +13,20 @@ var z_data = [
 // creating data objects..
 var data_z1 = {z: z_data, type: 'surface'};
 
+var layout = {
+	scene: {
+		xaxis: {
+			tickvals: [0, 1, 2, 3, 4, 5, 6, 7],
+			ticktext: ['乾', '兌', '離', '震', '巽', '坎', '艮', '坤'],
+			tickfont: { size: 16 }
+		},
+		yaxis: {
+			tickvals: [0, 1, 2, 3, 4, 5, 6, 7],
+			ticktext: ['乾', '兌', '離', '震', '巽', '坎', '艮', '坤'],
+			tickfont: { size: 16 }
+		}
+	}
+};
+
 // Plotting the surfaces..
-Plotly.newPlot('myDiv', [data_z1]);
+Plotly.newPlot('myDiv', [data_z1], layout);
