8ed6458152c70dc3054589303611f4644887d8aa
Wu Tien Yuan <Michele@WutekiMacBook-Air.local>
2018-05-05 21:13:29 +0800

    update code

--- client/view/index.js
+++ client/view/index.js
@@ -1,32 +1,51 @@
-var z_data = [
-	[ 0, 0, 0, 0, 0, 0, 0, 0],
-	[ 0, 0, 0, 0, 0, 0, 1, 0],
-	[ 0, 0, 0, 0, 0, 1, 0, 0],
-	[ 0, 0, 0, 0, 0, 0, 0, 0],
-	[ 0, 0,	0, 1, 0, 0, 0, 0],
-	[ 0, 0, 0, 0, 0, 0, 0, 0],
-	[ 0, 0, 0, 0, 0, 0, 0, 0],
-	[ 1, 0, 0, 0, 0, 0, 0, 0]
-]
+function getParameterByName(name, url) {
+	if (!url) url = window.location.href;
+	name = name.replace(/[\[\]]/g, "\\$&");
+	var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+		results = regex.exec(url);
+	if (!results) return null;
+	if (!results[2]) return '';
+	return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
 
+function get3dChart(id, table) {
+	var url = 'http://localhost:8080/v1/chart3d?id=' + id + '&table=' + table
+	$.getJSON(url, function(data){
 
-// creating data objects..
-var data_z1 = {z: z_data, type: 'surface'};
+		/*
+	var z_data = [
+		[ 0, 0, 0, 0, 0, 0, 0, 0],
+		[ 0, 0, 0, 0, 0, 0, 1, 0],
+		[ 0, 0, 0, 0, 0, 1, 0, 0],
+		[ 0, 0, 0, 0, 0, 0, 0, 0],
+		[ 0, 0,	0, 1, 0, 0, 0, 0],
+		[ 0, 0, 0, 0, 0, 0, 0, 0],
+		[ 0, 0, 0, 0, 0, 0, 0, 0],
+		[ 1, 0, 0, 0, 0, 0, 0, 0]
+	]
+	*/
+		var z_data = data.datas;
 
-var layout = {
-	scene: {
-		xaxis: {
-			tickvals: [0, 1, 2, 3, 4, 5, 6, 7],
-			ticktext: ['乾', '兌', '離', '震', '巽', '坎', '艮', '坤'],
-			tickfont: { size: 16 }
-		},
-		yaxis: {
-			tickvals: [0, 1, 2, 3, 4, 5, 6, 7],
-			ticktext: ['乾', '兌', '離', '震', '巽', '坎', '艮', '坤'],
-			tickfont: { size: 16 }
-		}
-	}
-};
+		// creating data objects..
+		var data_z1 = {z: z_data, type: 'surface'};
 
-// Plotting the surfaces..
-Plotly.newPlot('myDiv', [data_z1], layout);
+		var layout = {
+			scene: {
+				xaxis: {
+					tickvals: [0, 1, 2, 3, 4, 5, 6, 7],
+					ticktext: ['乾', '兌', '離', '震', '巽', '坎', '艮', '坤'],
+					tickfont: { size: 16 }
+				},
+				yaxis: {
+					tickvals: [0, 1, 2, 3, 4, 5, 6, 7],
+					ticktext: ['乾', '兌', '離', '震', '巽', '坎', '艮', '坤'],
+					tickfont: { size: 16 }
+				}
+			}
+		};
+
+		// Plotting the surfaces..
+		Plotly.newPlot('myDiv', [data_z1], layout);
+
+	});
+}
