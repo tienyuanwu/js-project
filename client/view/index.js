ad936708b3f17514d31b0a047f92ca4c1ce9503f
Wu Tien Yuan <Michele@WutekiMacBook-Air.local>
2018-05-12 18:29:00 +0800

    update code

--- client/view/index.js
+++ client/view/index.js
@@ -9,43 +9,107 @@ function getParameterByName(name, url) {
 }
 
 function get3dChart(id, table) {
-	var url = 'http://localhost:8080/v1/chart3d?id=' + id + '&table=' + table
+	var url = 'http://localhost:8080/v1/chart/sequence?id=' + id + '&table=' + table
 	$.getJSON(url, function(data){
-
-		/*
-	var z_data = [
-		[ 0, 0, 0, 0, 0, 0, 0, 0],
-		[ 0, 0, 0, 0, 0, 0, 1, 0],
-		[ 0, 0, 0, 0, 0, 1, 0, 0],
-		[ 0, 0, 0, 0, 0, 0, 0, 0],
-		[ 0, 0,	0, 1, 0, 0, 0, 0],
-		[ 0, 0, 0, 0, 0, 0, 0, 0],
-		[ 0, 0, 0, 0, 0, 0, 0, 0],
-		[ 1, 0, 0, 0, 0, 0, 0, 0]
-	]
-	*/
-		var z_data = data.datas;
-
-		// creating data objects..
-		var data_z1 = {z: z_data, type: 'surface'};
+		var array = []
+		for (i=0; i<data.datas.length; i++) {
+			var d = data.datas[i];
+			var color = 'rgb(' + d.color.r + ',' + d.color.g + ',' + d.color.b + ')';
+			console.log(d);
+			console.log(color);
+			var trace1 = {
+				x:[d.x], y: [d.y], z:[d.z],
+				showlegend: false,
+				mode: 'markers',
+				marker: {
+					size: 10,
+					color: color,
+				},
+				type: 'scatter3d'
+			};
+			array.push(trace1);
+		}
 
 		var layout = {
+			margin: {
+				l: 0,
+				r: 0,
+				b: 0,
+				t: 0
+			},
 			scene: {
 				xaxis: {
+					range: [0, 7],
+					tickmode: 'array',
 					tickvals: [0, 1, 2, 3, 4, 5, 6, 7],
 					ticktext: ['乾', '兌', '離', '震', '巽', '坎', '艮', '坤'],
 					tickfont: { size: 14 }
 				},
 				yaxis: {
+					range: [0, 7],
+					tickmode: 'array',
 					tickvals: [0, 1, 2, 3, 4, 5, 6, 7],
 					ticktext: ['乾', '兌', '離', '震', '巽', '坎', '艮', '坤'],
 					tickfont: { size: 14 }
 				}
 			}
+
 		};
 
 		// Plotting the surfaces..
-		Plotly.newPlot('myDiv', [data_z1], layout);
+		Plotly.newPlot('chart3dsequence', array, layout);
+	});
+}
 
+function getPieChart(id, table) {
+	var url = 'http://localhost:8080/v1/chart/frequency?id=' + id + '&table=' + table
+	$.getJSON(url, function(data){
+		var array = []
+		for (i=0; i<data.datas.length; i++) {
+			var d = data.datas[i];
+			var color = 'rgb(' + d.color.r + ',' + d.color.g + ',' + d.color.b + ')';
+			console.log(d);
+			console.log(color);
+			var trace1 = {
+				x:[d.x], y: [d.y], z:[d.z],
+				showlegend: false,
+				mode: 'markers',
+				marker: {
+					size: 10,
+					color: color,
+				},
+				type: 'scatter3d'
+			};
+			array.push(trace1);
+		}
+
+		var layout = {
+			margin: {
+				l: 0,
+				r: 0,
+				b: 0,
+				t: 0
+			},
+			scene: {
+				xaxis: {
+					range: [0, 7],
+					tickmode: 'array',
+					tickvals: [0, 1, 2, 3, 4, 5, 6, 7],
+					ticktext: ['乾', '兌', '離', '震', '巽', '坎', '艮', '坤'],
+					tickfont: { size: 14 }
+				},
+				yaxis: {
+					range: [0, 7],
+					tickmode: 'array',
+					tickvals: [0, 1, 2, 3, 4, 5, 6, 7],
+					ticktext: ['乾', '兌', '離', '震', '巽', '坎', '艮', '坤'],
+					tickfont: { size: 14 }
+				}
+			}
+
+		};
+
+		// Plotting the surfaces..
+		Plotly.newPlot('chart3dfrequency', array, layout);
 	});
 }
